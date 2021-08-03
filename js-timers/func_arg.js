4b9279f6e5a1e47dc34714fcb116ae8902ec9049
JC Ketterer <jcketterer18@gmail.com>
2021-08-02 20:18:21 -0700

    updating

--- js-timers/func_arg.js
+++ js-timers/func_arg.js
@@ -36,44 +36,44 @@ const double = makeMultiplyFunc(2)
 
 //Single Threading 
 
-// greet();
-// //This will block the thread and pop up in the browser
-// alert("I am an alert");
-// //Once you click ok the rest of the thread will run
-// diss(); 
+greet();
+//This will block the thread and pop up in the browser
+alert("I am an alert");
+//Once you click ok the rest of the thread will run
+diss(); 
 
-// greet();
-// setTimeout(diss, 3000);
-// greet();
+greet();
+setTimeout(diss, 3000);
+greet();
 
-// setInterval(diss, 2000)
+setInterval(diss, 2000)
 
 //Anonymous function
 
-// greet();
-// setTimeout(function() {
-//     diss();
-//     diss();
-//     diss();
-// }, 1000);
-// greet();
+greet();
+setTimeout(function() {
+    diss();
+    diss();
+    diss();
+}, 1000);
+greet();
 
-// setTimeout(function() {
-//     console.log("Meow")
-//     console.log("Woof")
-//     console.log("Oink")
-// }, 3000);
+setTimeout(function() {
+    console.log("Meow")
+    console.log("Woof")
+    console.log("Oink")
+}, 3000);
 
-// function doTwice(func) {
-//     func();
-//     func();
-// }
+function doTwice(func) {
+    func();
+    func();
+}
 
-// doTwice(function () {
-//     console.log("STOP BOTHERING ME!");
-//     console.log("Please Go AWAY!");
-// })
+doTwice(function () {
+    console.log("STOP BOTHERING ME!");
+    console.log("Please Go AWAY!");
+})
 
-// const printOne = function one() {
-//     console.log(1)
-// }
+const printOne = function one() {
+    console.log(1)
+}
